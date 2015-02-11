2981e83516c1ebb4c80154b9ead6d69469d21bb5
trikitrok <trikitrok@gmail.com>
2015-02-11 23:19:02 +0100

    Refactor: extracted explaining helper

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -3,6 +3,9 @@
 (defn- with-more-than-6-chars? [password]
   (> (count password) 6))
 
+(defn- at-least-one-upper-case-char-in [password]
+  (not= nil (some #(Character/isUpperCase %) password)))
+
 (defn valid? [password]
   (and (with-more-than-6-chars? password)
-       (not= nil (some #(Character/isUpperCase %) password))))
+       (at-least-one-upper-case-char-in password)))
