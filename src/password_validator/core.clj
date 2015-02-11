d6fec4e8f2afdf3ab7bf62324db140375a2d7513
trikitrok <trikitrok@gmail.com>
2015-02-11 23:31:23 +0100

    Refctor: reduced duplication among some helpers by introducing new any? function

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -3,17 +3,20 @@
 (defn- more-than-6-chars-in? [password]
   (> (count password) 6))
 
-(defn- at-least-one-upper-case-char-in [password]
-  (not= nil (some #(Character/isUpperCase %) password)))
+(defn- any? [pred coll]
+  (not= nil (some pred coll)))
 
-(defn- at-least-one-lower-case-char-in [password]
-  (not= nil (some #(Character/isLowerCase %) password)))
+(defn- at-least-one-upper-case-char-in? [password]
+  (any? #(Character/isUpperCase %) password))
 
-(defn- at-least-one-underscore-char-in [password]
-  (not= nil (some #{\_} password)))
+(defn- at-least-one-lower-case-char-in? [password]
+  (any? #(Character/isLowerCase %) password))
+
+(defn- at-least-one-underscore-char-in? [password]
+  (any? #{\_} password))
 
 (defn valid? [password]
   (and (more-than-6-chars-in? password)
-       (at-least-one-upper-case-char-in password)
-       (at-least-one-lower-case-char-in password)
-       (at-least-one-underscore-char-in password)))
+       (at-least-one-upper-case-char-in? password)
+       (at-least-one-lower-case-char-in? password)
+       (at-least-one-underscore-char-in? password)))
