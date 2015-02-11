8fab56f4ab4d79417d007eb724c9f204effc8667
trikitrok <trikitrok@gmail.com>
2015-02-11 23:20:01 +0100

    Refactor: renamed helper

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -1,11 +1,11 @@
 (ns password-validator.core)
 
-(defn- with-more-than-6-chars? [password]
+(defn- more-than-6-chars-in? [password]
   (> (count password) 6))
 
 (defn- at-least-one-upper-case-char-in [password]
   (not= nil (some #(Character/isUpperCase %) password)))
 
 (defn valid? [password]
-  (and (with-more-than-6-chars? password)
+  (and (more-than-6-chars-in? password)
        (at-least-one-upper-case-char-in password)))
