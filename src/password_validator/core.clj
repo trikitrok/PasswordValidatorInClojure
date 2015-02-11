3bd5e608bbffb71d4bf33f17ffec0d46e9c126d0
trikitrok <trikitrok@gmail.com>
2015-02-11 23:56:46 +0100

    Refactor: renamed all conditions to be a valid password

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -1,29 +1,29 @@
 (ns password-validator.core)
 
-(defn- more-than-6-chars-in? [password]
+(defn- has-more-than-6-chars? [password]
   (> (count password) 6))
 
 (defn- any? [pred coll]
   (not= nil (some pred coll)))
 
-(def ^:private at-least-one-upper-case-char-in?
+(def ^:private contains-at-least-one-upper-case-char?
   (partial any? #(Character/isUpperCase %)))
 
-(def ^:private at-least-one-lower-case-char-in?
+(def ^:private contains-at-least-one-lower-case-char?
   (partial any? #(Character/isLowerCase %)))
 
-(def ^:private at-least-one-underscore-char-in?
+(def ^:private contains-at-least-one-underscore-char?
   (partial any? #{\_}))
 
-(def ^:private at-least-one-numeric-char-in?
+(def ^:private contains-at-least-one-numeric-char?
   (partial any? #(Character/isDigit %)))
 
 (def ^:private conditions-to-be-valid 
-  [more-than-6-chars-in?
-   at-least-one-upper-case-char-in? 
-   at-least-one-lower-case-char-in?
-   at-least-one-underscore-char-in?
-   at-least-one-numeric-char-in?])
+  [has-more-than-6-chars?
+   contains-at-least-one-upper-case-char? 
+   contains-at-least-one-lower-case-char?
+   contains-at-least-one-underscore-char?
+   contains-at-least-one-numeric-char?])
 
 (defn valid? [password]
   (every? #(% password) conditions-to-be-valid))
